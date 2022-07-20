34599b5eec999b89453969db3b745f412a9a67c1
Thomas Pham <phamthomas19@gmail.com>
2022-07-20 13:02:50 -0500

    finished author routes

--- models/author.js
+++ models/author.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     }
+})
+
+authorSchema.pre('remove', function(callback){
+  Book.find({ author: this.id}, (err, books) =>{
+      if(err){
+          callback(err)
+      }else if (books.length > 0){
+          callback(new Error('This author has books still'))
+      }else{
+        callback()
+      }
+  })
 })
 
 module.exports = mongoose.model('Author', authorSchema)
